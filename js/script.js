209b6a1d61016c40595625d35f8cb8c6bfa200d9
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-03-10 15:25:57 +0300

    Cycle-styling for cards almost done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-window.onload = function() {
-    let card = document.getElementById('card1');
-    card.onmouseover = (event) => {
-        card.style.boxShadow = '0 15px 15px -10px rgba(255,0,0, 0.5)';
-        card.style.cursor = 'pointer';
-        card.style.width = '300px';
-        card.style.height = '200px';
-        document.getElementById('pic-1').style.marginLeft = '0';
-        document.getElementById('pic-1').style.marginRight = '0';
+window.onload = () => {
+    let crds = [card1, card2, card3, card4, card5];
+    for (let i = 0; i < crds.length; i++) {
+        crds[i].onmouseover = () => {
+            crds[i].style.boxShadow = '20px 20px 20px -20px rgba(255,0,0, 0.9)';
+            crds[i].style.cursor = 'pointer';
+            crds[i].style.height = '300px';
+        };
+        crds[i].onmouseout = () => {
+            crds[i].style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+            crds[i].style.height = '200px';
+        }
     }
+
 }
