ac2665d3001e7f54508211ec9c2ef71d5109cf15
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-03-19 19:43:51 +0300

    A lot of things done. Page has been fully reconcepted

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-window.onload = () => {
-    let crds = [card1, card2, card3, card4, card5];
-    for (let i = 0; i < crds.length; i++) {
-        crds[i].onmouseover = () => {
-            crds[i].style.boxShadow = '20px 20px 20px -20px rgba(255,0,0, 0.9)';
-            crds[i].style.cursor = 'pointer';
-            crds[i].style.height = '300px';
-        };
-        crds[i].onmouseout = () => {
-            crds[i].style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
-            crds[i].style.height = '200px';
-        }
-    }
+$(window).ready(function() {
+    let owl = $(".owl-carousel");
+    owl.owlCarousel({
+        items: 1,
+        margin: 10,
+        loop: true,
+        nav: true
+    });
 
-}
+    $('.navbar-item').mouseover(function() {
+        $(this).addClass('animated pulse faster')
+    });
+    $('.navbar-item').mouseout(function() {
+        $(this).removeClass('animated pulse faster')
+    });
+})
